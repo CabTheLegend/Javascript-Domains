7014ee7a18edea9d6a87e99d0de93c6ebc998fd4
CabTheLegend <158610305+CabTheLegend@users.noreply.github.com>
2024-04-14 16:10:29 -0400

    Practice_1

---
+++ Practice 1/script.js
@@ -0,0 +1,12 @@
+let myVariable = 5;
+console.log(myVariable);
+myVariable += 5;
+console.log(myVariable);
+
+const add_this = 2;
+
+function incrementNumber(initial, incrementor) {
+    return initial + incrementor;
+}
+
+console.log(incrementNumber(myVariable, add_this));
