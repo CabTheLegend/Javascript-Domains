9e27c4a087602a72bf9d8e98c97c85e63990bbac
CabTheLegend <158610305+CabTheLegend@users.noreply.github.com>
2024-04-14 20:25:14 -0400

    update

---
+++ Practice 2/script.js
@@ -0,0 +1,17 @@
+let x = true;
+let y = false;
+
+if (x === true) {
+} else if (x == "true") {
+  console.log("x is equal to 'true'");
+}
+
+let a = Math.ceil(Math.sin(2));
+let z = Math.abs(-18);
+
+console.log(a);
+console.log(z);
+
+let final_value = Math.round(Math.sqrt(a + z));
+
+console.log("The final value is:", final_value);
